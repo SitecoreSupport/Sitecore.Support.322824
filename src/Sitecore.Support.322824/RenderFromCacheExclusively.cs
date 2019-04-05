f23a804c955a0fbf7390b3cdd839cbf921315b98
iro <iro@sitecore.net>
2019-04-05 18:01:22 +0300

    Initial for real

--- src/Sitecore.Support.322824/RenderFromCacheExclusively.cs
+++ src/Sitecore.Support.322824/RenderFromCacheExclusively.cs
@@ -1,19 +1,69 @@
 ﻿namespace Sitecore.Support.XA.Foundation.Presentation.Pipelines.RenderRendering
 {
+  using Sitecore.Caching;
+  using Sitecore.Mvc.Extensions;
   using Sitecore.Mvc.Pipelines.Response.RenderRendering;
+  using Sitecore.Mvc.Presentation;
+  using Sitecore.Sites;
   using Sitecore.XA.Foundation.Abstractions;
+  using Sitecore.XA.Foundation.Multisite.Extensions;
   using Sitecore.XA.Foundation.Presentation.Services;
+  using System;
+  using System.IO;
 
-  public class RenderFromCacheExclusively : Sitecore.XA.Foundation.Presentation.Pipelines.RenderRendering.RenderFromCacheExclusively
+  public class RenderFromCacheExclusively : RenderFromCache
   {
-    public RenderFromCacheExclusively(ICacheTokenProcessingService caheCacheTokenProcessingService, IContext context) : base(caheCacheTokenProcessingService, context)
+    private readonly ICacheTokenProcessingService _cacheTokenProcessingService;
+
+    protected IContext Context
     {
+      get;
+    }
+
+    public RenderFromCacheExclusively(ICacheTokenProcessingService caheCacheTokenProcessingService, IContext context)
+    {
+      _cacheTokenProcessingService = caheCacheTokenProcessingService;
+      Context = context;
     }
 
     public override void Process(RenderRenderingArgs args)
     {
       args.CacheKey = args.CacheKey + "_#id:" + args.Rendering.UniqueId.ToString();
       base.Process(args);
+    }
+
+    protected override bool Render(string cacheKey, TextWriter writer, RenderRenderingArgs args)
+    {
+      if (!Context.Site.IsSxaSite())
+      {
+        return base.Render(cacheKey, writer, args);
+      }
+      HtmlCache htmlCache = Context.Site.ValueOrDefault((SiteContext site) => CacheManager.GetHtmlCache(site));
+      if (htmlCache == null)
+      {
+        return false;
+      }
+      string html = htmlCache.GetHtml(cacheKey);
+      if (html == null)
+      {
+        return false;
+      }
+      IDisposable item = RenderingContext.EnterContext(args.Rendering);
+      args.Disposables.Add(item);
+      string html2 = ProcessPlaceholders(html, args);
+      html2 = ProcessRenderings(html2, args);
+      writer.Write(html2);
+      return true;
+    }
+
+    protected virtual string ProcessPlaceholders(string html, RenderRenderingArgs args)
+    {
+      return _cacheTokenProcessingService.ProcessPlaceholderToken(html, args);
+    }
+
+    protected virtual string ProcessRenderings(string html, RenderRenderingArgs args)
+    {
+      return _cacheTokenProcessingService.ProcessRenderingToken(html, args);
     }
   }
 }
