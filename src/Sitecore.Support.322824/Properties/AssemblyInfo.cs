0f04a1e654e66a7aee81780c1d9fda09d482d70f
iro <iro@sitecore.net>
2019-04-05 17:50:15 +0300

    Initial

---
+++ src/Sitecore.Support.322824/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.322824")]
+[assembly: AssemblyProduct("Sitecore.Support.322824")]
+[assembly: ComVisible(false)]
---
+++ src/Sitecore.Support.322824/RenderFromCacheExclusively.cs
@@ -0,0 +1,19 @@
+﻿namespace Sitecore.Support.XA.Foundation.Presentation.Pipelines.RenderRendering
+{
+  using Sitecore.Mvc.Pipelines.Response.RenderRendering;
+  using Sitecore.XA.Foundation.Abstractions;
+  using Sitecore.XA.Foundation.Presentation.Services;
+
+  public class RenderFromCacheExclusively : Sitecore.XA.Foundation.Presentation.Pipelines.RenderRendering.RenderFromCacheExclusively
+  {
+    public RenderFromCacheExclusively(ICacheTokenProcessingService caheCacheTokenProcessingService, IContext context) : base(caheCacheTokenProcessingService, context)
+    {
+    }
+
+    public override void Process(RenderRenderingArgs args)
+    {
+      args.CacheKey = args.CacheKey + "_#id:" + args.Rendering.UniqueId.ToString();
+      base.Process(args);
+    }
+  }
+}
